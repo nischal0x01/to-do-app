65419204b49f6933f3d23024266f5802286eb54b
Nischal Subedi <nischal1337@gmail.com>
2024-05-17 23:01:59 +0545

    add: configuring zod

---
+++ backend/types.js
@@ -0,0 +1,11 @@
+const zod = require('zod');
+
+const createToDo = zod.object({
+    title: zod.string(),
+    description: zod.string()
+})
+
+const updateTodo = zod.object({
+    id: zod.string()
+})
+
